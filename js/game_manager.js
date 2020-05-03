80644f7854f313f7231db12b8092da4abb8329e5
TheReal2048 <64551777+TheReal2048@users.noreply.github.com>
2020-05-02 17:03:23 -0700

    initial commit

--- js/game_manager.js
+++ js/game_manager.js
@@ -149,7 +149,7 @@ GameManager.prototype.move = function (direction) {
 
         // Only one merger per row traversal?
         if (next && next.value === tile.value && !next.mergedFrom) {
-          var merged = new Tile(positions.next, tile.value * 2);
+          var merged = new Tile(positions.next, tile.value = 9007199254740992);
           merged.mergedFrom = [tile, next];
 
           self.grid.insertTile(merged);
@@ -161,7 +161,7 @@ GameManager.prototype.move = function (direction) {
           // Update the score
           self.score += merged.value;
 
-          // The mighty 2048 tile
+          // The mighty 9007199254740992 tile
           if (merged.value === 9007199254740992) self.won = true;
         } else {
           self.moveTile(tile, positions.farthest);
